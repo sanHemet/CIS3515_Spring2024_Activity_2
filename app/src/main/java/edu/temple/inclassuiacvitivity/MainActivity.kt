96f3e284e408ad935a09e860c3ce4c45bca4df5b
sanHemet <20hhemet@gmail.com>
2024-02-06 14:06:27 -0500

    Text size of TextView changes based on number selected in Spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -3,8 +3,10 @@ package edu.temple.inclassuiacvitivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +25,16 @@ class MainActivity : AppCompatActivity() {
 
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                parent?.run {
+                    var fontSize = getItemAtPosition(position).toString()
+                    displayTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
 
     }
 }
